dac85be2b06420ffe0f389cd4412f14d12ff8012
victorwang1 <victor.wang@me.com>
2017-09-11 16:17:47 -0700

    dynamically showing videoList

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,7 @@
 angular.module('video-player')
-
 .component('app', {
-  // TODO
+  controller: function() {
+    this.videos = window.exampleVideoData;
+  },
+  templateUrl: "src/templates/app.html"
 });
--- src/components/videoPlayer.js
+++ src/components/videoPlayer.js
@@ -1,5 +1,5 @@
 angular.module('video-player')
 
 .component('videoPlayer', {
-  // TODO
+  templateUrl: "src/templates/videoPlayer.html"
 });
