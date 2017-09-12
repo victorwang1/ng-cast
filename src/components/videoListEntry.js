f7461f642130e1452d3a6ad0986319f05e471f2b
victorwang1 <victor.wang@me.com>
2017-09-11 20:04:13 -0700

    workinggggggggg

--- src/components/videoListEntry.js
+++ src/components/videoListEntry.js
@@ -6,11 +6,12 @@ angular.module('video-player')
   },
   controller: function() {
     this.handleClick = () => {
+      console.log('hey');
       this.setPlayerVideo(this.video);
     };
-    this.$onInit = function() {
-      console.log(this.setPlayerVideo);
-    }
+    // this.$onInit = function() {
+    //   console.log(this.setPlayerVideo);
+    // }
   },
 
   templateUrl: "src/templates/videoListEntry.html"
